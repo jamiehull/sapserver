9a413abefb9f45bc89fb4c17560db8d96bcc0a05
jamiehull <jimjam.hull@gmail.com>
2026-02-19 22:14:06 +0000

    build: modified addexternal.cmake to ignore duplicate targets

--- addexternal.cmake
+++ addexternal.cmake
@@ -1,15 +1,25 @@
 include(FetchContent)
 
 function(add_external_library name dir repo tag build)
+
+# Check if cmake target definition already exists
+    if(TARGET ${name})
+        message(STATUS "Target ${name} already exists, skipping")
+        return()
+    endif()
+
+# If it does not exist - clone it
 	if(NOT EXISTS ${dir}/CMakeLists.txt AND NOT EXISTS ${dir}/README.md)
 		message(STATUS "Cloning ${name} from ${repo} to ${dir}")
 		if(tag)
-			FetchContent_Declare(${name} GIT_REPOSITORY ${repo} GIT_TAG ${tag} SOURCE_DIR ${dir})
+			FetchContent_Declare(${name} GIT_REPOSITORY ${repo} GIT_TAG ${tag} GIT_SUBMODULES "" SOURCE_DIR ${dir})
 		else()
-			FetchContent_Declare(${name} GIT_REPOSITORY ${repo} SOURCE_DIR ${dir})
+			FetchContent_Declare(${name} GIT_REPOSITORY ${repo} GIT_SUBMODULES "" SOURCE_DIR ${dir})
 		endif()
 		FetchContent_MakeAvailable(${name})
 	endif()
+	
+# If it does exist use the local lib
 	if(EXISTS ${dir}/CMakeLists.txt)
 		message(STATUS "Using existing ${name} at ${dir}")
 		if(${build})
